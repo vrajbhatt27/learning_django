13f1ac98ed86ba9b1e8f9697bf14682ce54adf94
vraj <vrajbhatt27@gmail.com>
2022-01-30 22:49:39 +0530

    learning app complete with auth

---
+++ auth_module/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CreateUserForm
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+
+
+        context = {'form': form}
+        return render(request, 'auth_module/signUp.html', context)
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('polls:index')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+
+        context = {}
+        return render(request, 'auth_module/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'auth_module/home.html')
